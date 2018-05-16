217759940e395b43f5a4482e40f4712fb156ae35
luckygro <lukas@luckyg.de>
2018-05-16 19:01:37 +0200

    test travis 2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,6 +6,7 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <p>Happy you are here</p>
+    <p>one more line</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
